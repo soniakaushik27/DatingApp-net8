37be9a0e0773bf32a4930486260fa862c11fb541
soniakaushik27 <154929919+soniakaushik27@users.noreply.github.com>
2025-01-27 23:56:17 +0530

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,21 +10,11 @@ builder.Services.AddDbContext<DataContext>(opt=>
 {
    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
 });
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-// builder.Services.AddOpenApi();
+
+builder.Services.AddCors();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.MapOpenApi();
-// }
-
-// app.UseHttpsRedirection();
-
-// app.UseAuthorization();
-
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  http=inject(HttpClient);
+  title = 'Dating App';
+  users:any;
+  ngOnInit(): void {
+    this.http.get('http://localhost:5000/api/users').subscribe({
+     next:response=>this.users=response,
+     error:error=>console.log(error),
+     complete:()=>console.log('Request has been completed') 
+    })
+  }
+}
